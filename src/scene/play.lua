de56c40faa47c2924b19715438cc9eaa2b6a497c
Azarias Boutin <azarias.boutin@gmail.com>
2019-09-13 11:56:47 +0200

    keys

--- src/scene/play.lua
+++ src/scene/play.lua
@@ -13,9 +13,12 @@ local PlayScene = Scene:extend()
 function PlayScene:new()
     PlayScene.super.new(self)
     self.noteImage = love.graphics.newImage('res/note.png')
+    self.gKeyImage = love.graphics.newImage('res/sol.png')
+    self.fKeyImage = love.graphics.newImage('res/fa.png')
     assert(self.noteImage, 'Failed to load image')
-    Scene.addentity(self, Note, 2, self.noteImage)
+    Scene.addentity(self, Note, 1, self.noteImage)
     self.progress = 0
+    self.limitLine = 200
 end
 
 
@@ -25,8 +28,10 @@ function PlayScene:draw()
     love.graphics.setColor(1,1,1)
     love.graphics.rectangle('fill', 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
 
-    love.graphics.setPointSize(100)
     local middle = love.graphics.getHeight() / 3
+    love.graphics.draw(self.gKeyImage, 0, middle - 5, 0, 0.8)
+    love.graphics.setColor(0.1,0.1,0.3)
+    love.graphics.line(self.limitLine , 0, self.limitLine, love.graphics.getHeight())
     love.graphics.setColor(0,0,0)
     for i = 1,5 do
         local ypos = middle + 20 * i
@@ -45,7 +50,7 @@ end
 
 function PlayScene:update(dt)
     Scene.update(self, dt)
-    self.progress = self.progress + (dt *  10)
+    self.progress = self.progress + (dt *  50)
     -- if math.random() == 1 then
     --     self.notes[#self.notes+1] = Note(self, 20, 100)
     -- end

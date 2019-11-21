d8a8c49e60df23a25a9704c8d7ed05e395819cb8
Azarias Boutin <azarias.boutin@gmail.com>
2019-11-21 14:45:35 +0100

    Safer Config object Using interal object instead of using Config object itself

--- src/data/Config.lua
+++ src/data/Config.lua
@@ -5,6 +5,7 @@ local Theme = require('src.utils.Theme')
 
 local Config = {}
 
+local _configData = {}
 
 local function getSimpleLocale()
     local l = os.setlocale()
@@ -13,38 +14,37 @@ end
 
 
 function Config.parse()
-    getSimpleLocale()
     local conf = {}
     if love.filesystem.getInfo(Vars.configSave) then
         conf = lume.deserialize(love.filesystem.read(Vars.configSave))
         local allPrefs = Vars.userPreferences
         for k,v in pairs(allPrefs) do
-            Config[k] = conf[k] or v[1]
+            _configData[k] = conf[k] or v[1]
         end
     else
         conf = Vars.userPreferences
         for k,v in pairs(conf) do
-            Config[k] = v[1]
+            _configData[k] = v[1]
         end
         -- trying to get the computer locale
         local l = getSimpleLocale()
         if lume.find(Vars.userPreferences.lang, l) then
-            Config.lang = l
+            _configData.lang = l
         end
     end
     Config.updateSound()
 end
 
 function Config.updateSound()
-    love.audio.setVolume(Config.sound == 'off' and 0 or 1)
+    love.audio.setVolume(_configData.sound == 'off' and 0 or 1)
 end
 
 ---@param key string the key config to update
 ---@param value any the value to set
 ---@return boolean true if the value was updated, false otherwise
 function Config.update(key, value)
-    if Config[key] == value then return false end
-    Config[key] = value
+    if _configData[key] == value then return false end
+    _configData[key] = value
     if key == "sound" then Config.updateSound() end
     if key == "lang" then i18n.setLocale(value) end
     if key == "theme" then Theme.updateTheme(value) end
@@ -52,15 +52,19 @@ function Config.update(key, value)
     return true
 end
 
-local function isFunction(x)
-    return type(x) == "function"
-end
-
 function Config.save()
-    love.filesystem.write(Vars.configSave , lume.serialize(lume.reject(Config, isFunction, true)))
+    love.filesystem.write(Vars.configSave , lume.serialize(_configData))
 end
 
-
-
-
-return Config
+return setmetatable(Config, {
+    __newindex = function()
+        error("Cannot directly access config, use Config.update instead")
+    end,
+    __index = function(table, k)
+        assert(type(k) == "string", "Accessed property must be a string")
+        local conf = rawget(table, k)
+        if conf then return conf end
+        if _configData[k] then return _configData[k] end
+        error("Configuration " .. k .. " not found")
+    end
+})

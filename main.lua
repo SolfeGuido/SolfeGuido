70dfce5199201f73e3b5cb74493e5ae3f87f51e1
Azarias Boutin <azarias.boutin@gmail.com>
2019-11-22 14:20:06 +0100

    Custom run function for fixed timestep and save at the end of the game and no 'ifs' everywhere

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@ local ScreenManager = require('lib.ScreenManager')
 local Mobile = require('src.utils.Mobile')
 local Theme = require('src.utils.Theme')
 local Config = require('src.data.Config')
+local StatisticsManager = require('src.data.StatisticsManager')
 
 --- BEGIN DEBUG
 local debugGraph = require('lib.debugGraph')
@@ -23,6 +24,58 @@ function love.load()
     fpsGraph = debugGraph:new('fps', love.graphics.getWidth() - 200, love.graphics.getHeight() - 100 , 200);
     memoryGraph = debugGraph:new('mem', love.graphics.getWidth() - 200, love.graphics.getHeight() - 50, 200)
 --- END DEBUG
+end
+
+function love.run()
+	love.load(love.arg.parseGameArguments(arg), arg)
+    love.math.setRandomSeed(os.time())
+	-- We don't want the first frame's dt to include time taken by love.load.
+	love.timer.step()
+ 
+    local dt = 0
+    local fixed_dt = 1/60
+    local accumulator = 0
+
+ 
+	-- Main loop time.
+	return function()
+		-- Process events.
+        love.event.pump()
+        for name, a,b,c,d,e,f in love.event.poll() do
+            if name == "quit" then
+                return love.quit(a or 0)
+            end
+            love.handlers[name](a,b,c,d,e,f)
+        end
+ 
+		-- Update dt, as we'll be passing it to update
+        dt = love.timer.step()
+        accumulator = accumulator + dt
+
+        while accumulator >= fixed_dt do
+            love.update(fixed_dt)
+            accumulator = accumulator - fixed_dt
+        end
+		-- Call update and draw
+		love.update(dt)
+ 
+		if love.graphics.isActive() then
+			love.graphics.origin()
+			love.graphics.clear(love.graphics.getBackgroundColor())
+ 
+			love.draw()
+ 
+			love.graphics.present()
+		end
+ 
+		love.timer.sleep(0.0001)
+	end
+end
+
+function love.quit(a)
+    Config.save()
+    StatisticsManager.save()
+    return a
 end
 
 function love.draw()

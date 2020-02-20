66c3efecb8b8da0082fb777d8ba62c879be4a109
Azarias Boutin <azarias.boutin@gmail.com>
2020-02-20 10:08:12 +0100

    Abstract Button comments

--- src/objects/AbstractButton.lua
+++ src/objects/AbstractButton.lua
@@ -1,12 +1,25 @@
 
 local Entity = require('src.Entity')
 
+--- This class is the parent class of all buttons type, the icon button, the text button
+--- the piano, the radio button, ..., It helps detect when the button is pressed,
+--- eases the state handling, and calls the right method when somethings happens
+--- On the desktop app, it changes the cursor to "hand" when it is hovered
+--- To make it easier to deal with animations, an method can be called to create the
+--- necessary animations
 ---@class AbstractButton : Entity
+---@field state string the button's current state ('neutral', 'pressed', 'hovered')
+---@field consumed boolean wether the button was pressed or not
+---@field animataion number? the id of the current animation, if there is one runing
+---@field fingerID number? the id of the finger that pressed the button (used for the mobile app)
 local AbstractButton = Entity:extend()
 
 AbstractButton.defaultCursor = love.mouse.getSystemCursor('arrow')
 AbstractButton.hoveredCursor = love.mouse.getSystemCursor('hand')
 
+--- Constructor
+---@param container EntityContainer this button's container
+---@param options table the entities options
 function AbstractButton:new(container, options)
     Entity.new(self, container, options)
     self.state = "neutral"
@@ -16,6 +29,8 @@ function AbstractButton:new(container, options)
     self.fingerId = nil
 end
 
+--- Cancels the current animation and starts a new one
+--- use it to animate the button, without conflicts with other animations
 function AbstractButton:animate(...)
     if self.animation then
         self.timer:cancel(self.animation)
@@ -24,6 +39,7 @@ function AbstractButton:animate(...)
     self.animation = self.timer:tween(...)
 end
 
+--- Deletes the button
 function AbstractButton:dispose()
     if self.text then
         self.text:release()
@@ -32,12 +48,28 @@ function AbstractButton:dispose()
     AbstractButton.super.dispose(self)
 end
 
+--- Called when the user presses
+--- this button
 function AbstractButton:pressed() end
+
+--- Called when the mouse leaves this button
 function AbstractButton:leave() end
+
+--- Called when the user presses and releases
+--- this button
 function AbstractButton:onclick() end
+
+--- Called when the user releases the button
+--- (but might not have pressed it)
 function AbstractButton:released() end
+
+--- Called when the mouse enters the button
 function AbstractButton:hovered() end
 
+--- Private method, called to check if
+--- The onclick method should be caleld
+--- whenever the user clicks the button
+---@return boolean wether the onclick method was actually called
 function AbstractButton:handleClick()
     if not self.consumed then
         self.consumed = true
@@ -47,11 +79,21 @@ function AbstractButton:handleClick()
     return false
 end
 
+--- Default function to test if the button
+--- contains the given coordinates
+---@param x number x coordinate
+---@param y number y coordinate
+---@return boolean wether the button contains the given point
 function AbstractButton:contains(x, y)
     return self.x <= x and (self.x + self.width) >= x and
             self.y <= y and (self.y + self.height) >= y
 end
 
+--- Called whenever a finger presses the screen
+--- could be optimized with a tree or something though
+---@param id number id of the finger that pressed the screen
+---@param x number x coordinate
+---@param y number y coordinate
 function AbstractButton:touchpressed(id, x, y)
     if self.fingerId then return false end-- can't be pressed by two touches
     if self:contains(x, y) then
@@ -63,6 +105,10 @@ function AbstractButton:touchpressed(id, x, y)
     return false
 end
 
+--- Called whenever a finger is released from the screen
+---@param id number id of the finger released from the screen
+---@param x number x coordinate of the release
+---@param y number y coordinate of the release
 function AbstractButton:touchreleased(id, x, y)
     if not self.fingerId or id ~= self.fingerId then return false end
     self.fingerId = nil
@@ -75,6 +121,11 @@ function AbstractButton:touchreleased(id, x, y)
     return false
 end
 
+--- Called whenever the mouse moves on the screen
+--- checks the internal states, and see if the mouse
+--- enters or leave this button
+---@param x number
+---@param y number
 function AbstractButton:mousemoved(x, y)
     if love.mouse.isDown(1) then
         return false
@@ -95,6 +146,12 @@ function AbstractButton:mousemoved(x, y)
     return false
 end
 
+--- Called whenever a mouse button is pressed,
+--- will check if it is pressed inside the button
+--- and will update its state accordingly
+---@param x number x coordinate of the button
+---@param y number y coordinate of the button
+---@param button number id of the button that pressed the screen
 function AbstractButton:mousepressed(x, y, button)
     if button == 1 and (self.state == "neutral" or self.state == "hovered") then
         if self:contains(x, y) then
@@ -106,6 +163,15 @@ function AbstractButton:mousepressed(x, y, button)
     return false
 end
 
+--- Called whenever a mouse button is released
+--- will check if the release has been done inside the button
+--- if the left button of the mouse has been pressed AND released
+--- inside this button, and that it's not consummed, it will
+--- then call the 'onclick' method
+--- In all case, it will update the internal state of the button
+---@param x number x coordinate of the mouse
+---@param y number y coordinate of the mouse
+---@param button number id of the button released
 function AbstractButton:mousereleased(x, y, button)
     if button == 1 and self.state == "pressed" then
         self:released()

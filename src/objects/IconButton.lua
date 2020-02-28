bce6b1ea56dcfb187ad9dd8e4f0674beeed681ef
Azarias Boutin <azarias.boutin@gmail.com>
2020-02-28 12:04:32 +0100

    IconButton review

--- src/objects/IconButton.lua
+++ src/objects/IconButton.lua
@@ -5,9 +5,15 @@ local lume = require('lib.lume')
 -- Entities
 local AbstractButton = require('src.objects.AbstractButton')
 
+--- Widget consisting of an icon, and that can
+--- be pressed as a button
 ---@class IconButton : AbstractButton
+---@field image Image
+---@field shaking boolean
 local IconButton = AbstractButton:extend()
 
+---@param container EntityContainer
+---@param config table
 function IconButton:new(container, config)
     AbstractButton.new(self, container, config)
     local defaultFont = assets.fonts.Icons(config.size or Vars.titleSize)
@@ -35,6 +41,8 @@ function IconButton:new(container, config)
     end
 end
 
+--- Shakes the button, used
+--- for the sound icon
 function IconButton:shake()
     if self.shaking then return end
     local shakeTime = 0.1
@@ -50,50 +58,65 @@ function IconButton:shake()
 end
 
 
+--- Changes the text of the icon,
+--- thus changing the icon
+---@param icon string
 function IconButton:setIcon(icon)
     self.image:set(icon)
 end
 
+---@return number this button's width
 function IconButton:width()
     return self._width
 end
 
+--- Inherited method
 function IconButton:dispose()
     self.image:release()
     self.image = nil
     IconButton.super.dispose(self)
 end
 
+---@return string representation of this icon
 function IconButton:__tostring()
     return "IconButton(" .. tostring(self.image) .. ")"
 end
 
+---@param x number
+---@param y number
+---@return boolean wether the button contains the given point
 function IconButton:contains(x, y)
     return self.x <= x and (self.x + self._width) >= x and
             self.y <= y and (self.y + self.height) >= y
 end
 
+--- Animates the button
 function IconButton:hovered()
     self:animate(Vars.transition.tween, self, {color =  Theme.hovered}, 'out-expo')
 end
 
+--- Animates the button
 function IconButton:pressed()
     self:animate(Vars.transition.tween, self, {color = Theme.clicked}, 'out-expo')
 end
 
+--- Animates the button
 function IconButton:released()
     self:animate(Vars.transition.tween, self, {color = Theme.font}, 'out-expo')
 end
 
+--- Animates the button
 function IconButton:leave()
     self:animate(Vars.transition.tween, self, {color = Theme.font}, 'out-expo')
 end
 
+--- When the button is clicked
 function IconButton:onclick()
     assets.sounds.click:play()
     if self.callback then self.callback(self) end
 end
 
+--- Inherited function
 function IconButton:draw()
     love.graphics.setColor(self.color)
     if self.circled then

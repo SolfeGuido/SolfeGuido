73d3cb47bfcd7d6f873d3f18b8ff8ba3f7710ed6
Azarias Boutin <azarias.boutin@gmail.com>
2020-02-29 11:18:07 +0100

    Image review

--- src/objects/Image.lua
+++ src/objects/Image.lua
@@ -1,8 +1,17 @@
 local Entity = require('src.Entity')
 
+--- Used to draw images instead of icons,
+--- this is a simple object representation
+--- to simplify the handling of love images
+--- used for non-interactive images
 ---@class Image : Entity
+---@field xOrigin number
+---@field yOrigin number
+---@field rotation number
 local Image = Entity:extend()
 
+---@param container EntityContainer
+---@param options table
 function Image:new(container, options)
     Entity.new(self, container, options)
     if not self.scale and self.size then
@@ -14,6 +23,7 @@ function Image:new(container, options)
     self.yOrigin = self.image:getHeight() / 2
 end
 
+--- Inherited method
 function Image:draw()
     love.graphics.setColor(self.color)
     love.graphics.draw(self.image, self.x, self.y, self.rotation, self.scale, self.scale, self.xOrigin, self.yOrigin)

1946551b6cf4fa297f2e5bef0a70ac589c58f8b6
Azarias Boutin <azarias.boutin@gmail.com>
2020-03-02 12:04:12 +0100

    Line review

--- src/objects/Line.lua
+++ src/objects/Line.lua
@@ -2,13 +2,20 @@ local Entity = require('src.Entity')
 local Theme = require('src.utils.Theme')
 
 ---@class Line : Entity
+--- Really simple class to be able to
+--- animate the different lines of the game
+--- used do draw the measures
 local Line = Entity:extend()
 
+--- Constructor
+---@param container EntityContainer
+---@param options table
 function Line:new(container, options)
     Entity.new(self, container, options)
     self.color = options.color or Theme.font:clone()
 end
 
+--- Inherited method
 function Line:draw()
     love.graphics.setColor(self.color)
     love.graphics.setLineWidth(self.lineWidth or 1)

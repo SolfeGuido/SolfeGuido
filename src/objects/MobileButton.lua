ab86e61eb882f323021e04a0f0e40e3dbdbd6858
Azarias Boutin <azarias.boutin@gmail.com>
2020-03-04 09:42:44 +0100

    MobileButton review

--- src/objects/MobileButton.lua
+++ src/objects/MobileButton.lua
@@ -1,51 +1,68 @@
-
-
 local AbstractButton = require('src.objects.AbstractButton')
 local Theme = require('src.utils.Theme')
 
+--- Kind of a bad name for this button, used to give answers
+--- with simple note names, is an alternative to the piano notes
+--- consist only of a border and a text at the center
 ---@class MobileButton : AbstractButton
 local MobileButton = AbstractButton:extend()
 
+--- Constructor
+---@param container EntityContainer
+---@param options table
 function MobileButton:new(container, options)
     AbstractButton.new(self, container, options)
     self.color = options.color or Theme.font:clone()
 end
 
+--- Checks if the button contains
+--- the given coordinates
+---@param x number
+---@param y number
+---@return boolean
 function MobileButton:contains(x, y)
     local padding = Vars.mobileButton.padding
     return self.x <= x and (self.x + (self.width or (self.text:getWidth() + padding * 2))) >= x and
             self.y <= y and (self.y + (self.text:getHeight() + padding * 2)) >= y
 end
 
+---@return number the button with
 function MobileButton:getWidth()
     return (self.width or self.text:getWidth())
 end
 
+---@return number the button height
 function MobileButton:getHeight()
     return self.text:getHeight() + (Vars.mobileButton.padding * 2)
 end
 
+--- triggers the hover animation
 function MobileButton:hovered()
     self:animate(Vars.transition.tween, self, {color = Theme.hovered}, 'out-expo')
 end
 
+--- Triggers the leave animation
 function MobileButton:leave()
     self:animate(Vars.transition.tween, self, {color = Theme.font}, 'out-expo')
 end
 
+--- Triggers the pressed animation
 function MobileButton:pressed()
     self:animate(Vars.transition.tween, self, {color = Theme.clicked}, 'out-expo')
 end
 
+--- Triggers the released animation
 function MobileButton:released()
     self:animate(Vars.transition.tween, self, {color = Theme.font}, 'out-expo')
 end
 
+--- Calls the buttons callback and consumes it
 function MobileButton:onclick()
     if self.callback then self.callback() end
     self.consumed = false
 end
 
+--- Inherited method
 function MobileButton:draw()
     love.graphics.push()
     love.graphics.translate(self.x, self.y)

3fc6d2d73b6e9d4209fa8df50af7a33b21b3f1bc
Azarias Boutin <azarias.boutin@gmail.com>
2020-03-06 14:09:16 +0100

    RadioButton review

--- src/objects/RadioButtonGroup.lua
+++ src/objects/RadioButtonGroup.lua
@@ -1,13 +1,22 @@
 local EntityContainer = require('src.objects.EntityContainer')
 local RadioButton = require('src.objects.RadioButton')
 
----@class RadioButtonGroup : Entity
+--- To make it easier to handle the radio buttons group,
+--- this class handles the selected values, and can
+--- set a given value
+---@class RadioButtonGroup : EntityContainer
 local RadioButtonGroup = EntityContainer:extend()
 
+--- constructor
+---@param container EntityContainer
+---@param options table
 function RadioButtonGroup:new(container, options)
     EntityContainer.new(self, container, options)
 end
 
+--- Checks the given radio button
+--- and unchecks all the other ones
+---@param btn RadioButton
 function RadioButtonGroup:_buttonClicked(btn)
     btn.consumed = false
     for _,radio in ipairs(self._entities) do
@@ -19,6 +28,10 @@ function RadioButtonGroup:_buttonClicked(btn)
     end
 end
 
+--- Creates a radio button with the given options
+--- and add it to the group
+---@return RadioButton
+---@param options table
 function RadioButtonGroup:addRadioButton(options)
     if options.callback then
         local save = options.callback
@@ -32,6 +45,8 @@ function RadioButtonGroup:addRadioButton(options)
     return self:addEntity(RadioButton, options)
 end
 
+--- Access to the value of the selected button
+---@return string
 function RadioButtonGroup:getSelectedValue()
     for _,v in ipairs(self._entities) do
         if v.isChecked then return v.value end
@@ -39,6 +54,11 @@ function RadioButtonGroup:getSelectedValue()
     return nil
 end
 
+--- Changes the group to select the radio
+--- button with the given value
+--- none will be selected if the value is
+--- not contained by a single button
+---@param val string
 function RadioButtonGroup:setSelectedValue(val)
     for _, v in ipairs(self._entities) do
         if v.value == val then

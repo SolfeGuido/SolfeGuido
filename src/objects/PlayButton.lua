47f632e05c7d42bb21813e91379b890465692d17
Azarias Boutin <azarias.boutin@gmail.com>
2020-03-06 14:00:18 +0100

    PlayButton review

--- src/objects/PlayButton.lua
+++ src/objects/PlayButton.lua
@@ -3,9 +3,19 @@ local AbstractButton = require('src.objects.AbstractButton')
 local lume = require('lib.lume')
 local Theme = require('src.utils.Theme')
 
+--- The play button is quite special, when clicked, it triggers
+--- a unique animation, and loads another state, it's shaped
+--- is only used by this object
 ---@class PlayButton : Entity
+---@field private radius number
+---@field private color Color
+---@field private xOrigin number
+---@field private yOrigin number
 local PlayButton = AbstractButton:extend()
 
+--- Constructor
+---@param container EntityContainer
+---@param options table
 function  PlayButton:new(container, options)
     AbstractButton.new(self, container, options)
     self.image = love.graphics.newText(assets.fonts.Icons(Vars.mobileButton.fontSize),assets.IconName.Play)
@@ -15,10 +25,15 @@ function  PlayButton:new(container, options)
     self.yOrigin = self.image:getHeight() / 2
 end
 
+--- Checks wether the given point is inside the button
+---@return boolean
+---@param x number
+---@param y number
 function PlayButton:contains(x, y)
     return lume.distance(x, y, self.x, self.y, true) < self.radius * self.radius
 end
 
+--- Special animation when clicked
 function PlayButton:onclick()
     assets.sounds.click:play()
     local a = self.y * 2
@@ -33,6 +48,7 @@ function PlayButton:onclick()
     )
 end
 
+--- Inherited method
 function  PlayButton:draw()
     love.graphics.setColor(Theme.background)
     love.graphics.circle('fill', self.x, self.y, self.radius, 100)
